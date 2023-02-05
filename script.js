36b428ede47bc7dead4543de984c8a888fe54655
rbnccv <rbnccv@gmail.com>
2023-02-05 00:19:37 -0300

    En la

--- script.js
+++ script.js
@@ -25,11 +25,13 @@ window.addEventListener('load', function () {
 
       this.dx = 0
       this.dy = 0
+
+      this.speedModifier = 2
     }
 
     draw(context){
       context.beginPath()
-      context.arc(this.collisionX, this.collisionY, this.collisionRadio, 0, 2 * Math.PI)
+      context.arc(this.collisionX, this.collisionY, this.collisionRadio, 0, (2 * Math.PI))
 
       context.save()
       context.globalAlpha = .5
@@ -42,18 +44,35 @@ window.addEventListener('load', function () {
       context.lineTo(this.game.mouse.x, this.game.mouse.y)
       context.stroke()
 
-      console.log({x: this.collisionX,   y: this.collisionY});
-      console.log({x: this.game.mouse.x, y: this.game.mouse.y});
+      /* console.log({x: this.collisionX,   y: this.collisionY});
+      console.log({x: this.game.mouse.x, y: this.game.mouse.y}); */
     }
 
     update(){
-      this.speedX = (this.game.mouse.x - this.collisionX) / 20
-      this.speedY = (this.game.mouse.y - this.collisionY) / 20
+
+      this.dx = this.game.mouse.x - this.collisionX //dx es la (distancia) diferencia entre el mouse y el centro de colisión dentro del eje X
+      this.dy = this.game.mouse.y - this.collisionY
+
+      // Para una velocidad CONSTANTE se toma como valor de ddistancia la hipotenusa entre el eje X e Y (velocidad constante)
+      // distancia entre lasCONSTANTE dy y dx (hipotenusa)
+      const distance = Math.hypot(this.dy, this.dx)
+
+      if (distance > this.speedModifier){
+        this.speedX = this.dx / distance || 0
+        this.speedY = this.dy / distance || 0
+
+      }else{
+        this.speedX = 0
+        this.speedY = 0
+      }
       
-      this.collisionX += this.speedX
-      this.collisionY += this.speedY
+      this.collisionX += this.speedX * this.speedModifier // el speedModifier aumenta la velocidad de desplazamiento
+      this.collisionY += this.speedY * this.speedModifier
     }
   }
+
+
+  class Obstacle{}
 
   class Game {
     constructor(canvas){
@@ -83,9 +102,13 @@ window.addEventListener('load', function () {
       })
 
       canvas.addEventListener('mousemove', e => {
-        this.mouse.x = e.offsetX
-        this.mouse.y = e.offsetY
-        console.log(this.mouse);
+
+        if(this.mouse.pressed){
+          this.mouse.x = e.offsetX
+          this.mouse.y = e.offsetY
+        }
+
+        //console.log(this.mouse);
       })
     }
 
